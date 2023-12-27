55996c968de1211c203b6bb326573ae7a6c5e4b2
Quentin <>
2023-12-27 18:42:34 +0100

    adding components

--- angularcamingpitchbookingapp.client/src/app/app.module.ts
+++ angularcamingpitchbookingapp.client/src/app/app.module.ts
@@ -5,10 +5,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BasketComponent } from './basket/basket.component';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule, HttpClientModule,
