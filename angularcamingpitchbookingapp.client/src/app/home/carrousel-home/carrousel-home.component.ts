8d601f09114dfb1368fdd23dc1734457f8392a66
Quentin <>
2024-03-18 01:23:21 +0100

    fix issues

--- angularcamingpitchbookingapp.client/src/app/home/carrousel-home/carrousel-home.component.ts
+++ angularcamingpitchbookingapp.client/src/app/home/carrousel-home/carrousel-home.component.ts
@@ -1,7 +1,6 @@
 import { Component, Input, CUSTOM_ELEMENTS_SCHEMA, ViewChild } from '@angular/core';
 import { Slide } from './slide';
 import { CommonModule } from '@angular/common';
-import { SwiperContainer } from 'swiper/element';
 
 @Component({
   selector: 'app-carrousel-home',
@@ -14,6 +13,12 @@ import { SwiperContainer } from 'swiper/element';
 export class CarrouselHomeComponent {
   @Input() images: Slide[] = [];
   
-  @ViewChild('swiper-container', { static: false }) swiper?: SwiperContainer;  
-  
+  ngAfterContentInit(){
+    const swiperEl = document.querySelector('.swiper-pagination');
+    
+    if(swiperEl){
+      swiperEl.classList.add('pagination-position');
+    }
+
+  }
 }
