80e5836edcfd34217d063bd3387b0be70f9ce867
Quentin <>
2024-03-18 12:57:29 +0100

    styles header

--- angularcamingpitchbookingapp.client/src/app/home/carrousel-home/carrousel-home.component.ts
+++ angularcamingpitchbookingapp.client/src/app/home/carrousel-home/carrousel-home.component.ts
@@ -1,6 +1,7 @@
-import { Component, Input, CUSTOM_ELEMENTS_SCHEMA, ViewChild } from '@angular/core';
+import { Component, Input, CUSTOM_ELEMENTS_SCHEMA, ViewEncapsulation } from '@angular/core';
 import { Slide } from './slide';
 import { CommonModule } from '@angular/common';
+import Swiper from 'swiper';
 
 @Component({
   selector: 'app-carrousel-home',
@@ -12,13 +13,5 @@ import { CommonModule } from '@angular/common';
 })
 export class CarrouselHomeComponent {
   @Input() images: Slide[] = [];
-  
-  ngAfterContentInit(){
-    const swiperEl = document.querySelector('.swiper-pagination');
     
-    if(swiperEl){
-      swiperEl.classList.add('pagination-position');
-    }
-
-  }
 }
